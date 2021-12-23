11368ea0fd538774dfbb7000284c13e9aeefd793
azev77 <azev77@users.noreply.github.com>
2021-12-23 17:19:43 -0500

    Update DiscreteTime_Deterministic_POMDPs.jl

--- DiscreteTime_Deterministic_POMDPs.jl
+++ DiscreteTime_Deterministic_POMDPs.jl
@@ -5,16 +5,16 @@ K_SS = I_SS/δ
 
 if 1==1
     a_0(s;δ=δ) = δ*s
-    min_s = eps(); max_s = 2.0*K_SS; n_s = 200;
-    min_a = eps(); max_a = a_0(max_s); n_a = 400; # keeps a≥0 
+    min_s = eps(); max_s = 10.0*K_SS; n_s = 800;
+    min_a = eps(); max_a = K_SS; n_a = 200;
     "states:"
     states = range(min_s, max_s, length=n_s)
     ceil_state(s) = states[searchsortedfirst(states, s)]
     "actions:" 
     valid_actions()  = range(min_a, max_a, length=n_a)        # possible actions any state.
-    valid_actions(s) = filter(<(a0(s)), valid_actions())      # valid actions @ state=s 
+    valid_actions(s) = filter(>=(-(1-δ)*s), valid_actions())  # valid actions @ state=s 
     "Transition:" 
-    μ(s,a;δ=δ)      = a - δ*s     
+    μ(s,a;δ=δ)      = a +(1-δ)*s     
     "reward:" 
     r(s,a;z=z) = z*s -a -0.5*(a^2)
     "discount:"
@@ -41,36 +41,21 @@ if 1==1
     using Plots
     # Value
     plot(legend=:bottomright, title="Value Functions");
-    #plot!(states[2:end], i->A0 + A1 * log(i), lab="closed form") 
+    #plot!(states[2:end], i->A0 + A1 * i, lab="closed form") 
     plot!(states[2:end], i->value(sol1, i),   lab="sol1")
     plot!(states[2:end], i->value(sol2, i),   lab="sol2")
-    plot!(states[2:end], i->value(sol4, i),   lab="sol4")
-    plot!(states[2:end], i->value(sol3, i),   lab="sol3")
 
     # Simulation
-    Tsim=150; s0=states[2]; 
-    sim1 = []; push!(sim1, s0); 
+    Tsim=150; s0=0.5*K_SS; sim1 = []; push!(sim1, s0); 
     for tt in 1:Tsim
-        tt==1 ? s = s0 : nothing 
-        #a = valid_actions()[sol1.policy[searchsortedfirst(states, s)]]
-        a = action(sol1, s)
+        s = sim1[tt]
+        a = valid_actions()[sol1.policy[searchsortedfirst(states, s)]]
         sp = μ(s,a) 
         sp = ceil_state(sp)
         push!(sim1, sp)
-        s = sp
     end 
-
-    sim4 = []; push!(sim4, s0); 
-    for tt in 1:Tsim
-        tt==1 ? s = s0 : nothing 
-        a = action(sol4, s)
-        sp = μ(s,a) 
-        push!(sim4, sp)
-        s = sp
-    end 
-
+    
     plot(legend=:bottomright, title="Simulation");
     #plot!(simcf,   lab="closed form")
     plot!(sim1,   lab="sol1")
-    plot!(sim4,   lab="sol4")
 end
